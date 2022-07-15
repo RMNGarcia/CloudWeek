54b2cf3d815954b3016cc8513bee805d98f5a580
RMNGarcia <renatomngarcia@gmail.com>
2022-07-15 14:11:23 -0300

    initial commit

---
+++ application/s3-conf.php
@@ -0,0 +1,8 @@
+<?php
+$accessKey = 'AKIA2JHUIP4KMAUXM2UW';
+$secretKey = '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';
+$region = 'us-east-1';
+$bucket = 'scloud.local';
+$arqName =  'logo.jpg';
+$linkestatico = 'http://scloud.local.s3-website-us-east-1.amazonaws.com'
+?>
